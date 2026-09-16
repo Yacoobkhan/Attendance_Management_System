4cda36806a0baef5d44b2331e638f78b229fb90a
Yacoob <yacoobkhan18@gmail.com>
2026-09-16 15:51:38 +0530

    created employee model

--- attendance_system/employees/models.py
+++ attendance_system/employees/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Employee(models.Model):
+    employee_id = models.IntegerField(unique=True)
+    employee = models.CharField(max_length = 100)
+    dob = models.DateField()
+    role = models.CharField(max_length = 100)
+    phone = models.CharField(max_length = 15)
+    mail = models.EmailField(unique=True)
+    joining_date = models.DateField()
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f"{self.employee_id} - {self.employee}"
+    
+
