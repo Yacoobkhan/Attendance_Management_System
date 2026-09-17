b51f4e6f5f9ac252c7b8581330be4c9b0de40fb4
Yacoob <yacoobkhan18@gmail.com>
2026-09-17 11:10:59 +0530

    Implemented attendance history

--- attendance_system/attendance/views.py
+++ attendance_system/attendance/views.py
@@ -6,8 +6,22 @@ from rest_framework import generics
 # Create your views here.
 
 class AttendanceListView(generics.ListAPIView):
-    queryset= Attendance.objects.all()
+    # queryset= Attendance.objects.all()
     serializer_class = AttendanceSerializers
+
+    def get_queryset(self):
+        queryset = Attendance.objects.all().order_by('-date')
+
+        employee = self.request.query_params.get('employee')
+        date = self.request.query_params.get('date')
+
+        if employee:
+            queryset = queryset.filter(employee_id = employee)
+
+        if date:
+            queryset = queryset.filter(date = date)
+
+        return queryset
 
 class AttendanceCreateView(generics.CreateAPIView):
     queryset = Attendance.objects.all()
