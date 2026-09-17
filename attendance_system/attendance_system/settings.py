bee48b4e984342ba5dab8b2274884e77c062b319
Yacoob <yacoobkhan18@gmail.com>
2026-09-17 16:30:39 +0530

    initialized tailwind css

--- attendance_system/attendance_system/settings.py
+++ attendance_system/attendance_system/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
     'employees',
     'attendance',
     'leave',
@@ -47,6 +48,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -126,3 +128,11 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_URLS_REGEX = r'^/api/.*'
+
+CORS_ALLOWED_ORIGINS=[
+    'http://localhost:5173'
+]
+
+
